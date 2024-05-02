9773a839bb8c260fffdf6522abb2e422f4dc1110
chyyuu <chyyuu@users.noreply.github.com>
2024-05-02 13:02:10 +0000

    deploy: 6bbeb0e40bba69c30c653e10aca54e8a49b65c67

---
+++ ch1/os/logging/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["puts"],"struct":["Logger"]};
---
+++ ch1/os/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["kernel_interrupt","main"],"macro":["print","println"],"mod":["console","lang_items","logging"],"static":["HEAP_ALLOCATOR"]};
